05e0f741c7bb699106e02980f68e51b624813677
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-06-20 15:38:24 +0200

    Array of objects, todo

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 console.log("Vue ok,", Vue);
 
+// creating Vue app
 const { createApp } = Vue;
 
 createApp({
   data() {
-    return {};
+    return {
+      todoList: [
+        { text: "Complete homework", done: false },
+        { text: "Buy groceries", done: false },
+        { text: "Go for a run", done: false },
+        { text: "Call mom", done: false },
+        { text: "Finish project", done: false },
+        { text: "Read a book", done: false },
+        { text: "Pay bills", done: false },
+        { text: "Organize closet", done: false },
+        { text: "Schedule dentist appointment", done: false },
+        { text: "Write blog post", done: false },
+      ],
+    };
   },
+  //   mounting app
 }).mount("#root");
